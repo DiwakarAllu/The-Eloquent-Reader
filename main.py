217bc1d3d757902a8da86009fed193cb604b7bec
Allu Diwakar <101788832+DiwakarAllu@users.noreply.github.com>
2023-10-24 22:07:58 +0530

    Update main.py
    simple user interface is created

--- main.py
+++ main.py
@@ -1,28 +1,66 @@
 import pyttsx3
-from PyPDF2 import PdfReader
+import tkinter as tk
+from tkinter import filedialog
+import fitz  # PyMuPDF
 
-# Function to extract text from a PDF file
-def extract_text_from_pdf(pdf_file):
-    text = ""
-    pdf_reader = PdfReader(pdf_file)
+# Function to convert PDF to speech
+def convert_pdf_to_speech(pdf_path, speech_rate):
+    doc = fitz.open(pdf_path)
+    speaker = pyttsx3.init()
+    speaker.setProperty('rate', speech_rate)
 
-    for page in pdf_reader.pages:
-        text += page.extract_text()
+    for page_num in range(doc.page_count):
+        page = doc.load_page(page_num)
+        text = page.get_text()
+        speaker.say(text)
+        speaker.runAndWait()
 
-    return text
+# Function to browse for a PDF file
+def browse_pdf():
+    file_path = filedialog.askopenfilename(filetypes=[("PDF Files", "*.pdf")])
+    if file_path:
+        pdf_path.set(file_path)
 
-# Function to convert text to speech
-def text_to_speech(text):
-    engine = pyttsx3.init()
-    engine.setProperty('rate', 150)  # Adjust the speech rate (words per minute)
-    engine.say(text)
-    engine.runAndWait()
+# Function to convert and speak
+def convert_and_speak():
+    pdf_file = pdf_path.get()
+    rate = int(speech_rate.get())
+    if pdf_file:
+        convert_pdf_to_speech(pdf_file, rate)
+    else:
+        result_label.config(text="Please select a PDF file.")
 
-# Main function
-def main():
-    pdf_file_path = "C:\\Users\\Allu Diwakar\\Desktop\\AWS_Academy_Graduate___AWS_Academy_Cloud_Architecting_Badge20231012-45-ar5cdk.pdf"  # PDF file's path
-    text = extract_text_from_pdf(pdf_file_path)
-    text_to_speech(text)
+# Create the main application window
+app = tk.Tk()
+app.title("The Eloquent Reader")
 
-if __name__ == "__main__":
-    main()
+# Variables for PDF path and speech rate
+pdf_path = tk.StringVar()
+speech_rate = tk.StringVar()
+speech_rate.set(150)
+
+# Define colors for styling
+bg_color = "#F6F7F8"
+label_color = "#333333"
+button_color = "#6AC7F3"
+
+# Create and configure widgets
+file_label = tk.Label(app, text="Select PDF File:", bg=bg_color, fg=label_color)
+file_entry = tk.Entry(app, textvariable=pdf_path, width=40)
+browse_button = tk.Button(app, text="Browse", command=browse_pdf, bg=button_color, fg="white")
+rate_label = tk.Label(app, text="Speech Rate (words per minute):", bg=bg_color, fg=label_color)
+rate_entry = tk.Entry(app, textvariable=speech_rate)
+convert_button = tk.Button(app, text="Convert and Speak", command=convert_and_speak, bg=button_color, fg="white")
+result_label = tk.Label(app, text="", bg=bg_color, fg="red")
+
+# Place widgets in the window
+file_label.grid(row=0, column=0, padx=10, pady=10)
+file_entry.grid(row=0, column=1, padx=10, pady=10)
+browse_button.grid(row=0, column=2, padx=10, pady=10)
+rate_label.grid(row=1, column=0, padx=10, pady=10)
+rate_entry.grid(row=1, column=1, padx=10, pady=10)
+convert_button.grid(row=2, column=1, padx=10, pady=10)
+result_label.grid(row=3, column=1, padx=10, pady=10)
+
+# Start the main loop
+app.mainloop()
