4b31b8971c84a1351e1df2d7d4ac7cb1bb1d4c08
Allu Diwakar <101788832+DiwakarAllu@users.noreply.github.com>
2023-10-25 19:52:48 +0530

    Update main.py
    Interactive User Interface is added.

--- main.py
+++ main.py
@@ -30,6 +30,15 @@ def convert_and_speak():
     else:
         result_label.config(text="Please select a PDF file.")
 
+# Function to interpolate gradient color
+def gradient_color(start_color, end_color, fraction):
+    r1, g1, b1 = int(start_color[1:3], 16), int(start_color[3:5], 16), int(start_color[5:7], 16)
+    r2, g2, b2 = int(end_color[1:3], 16), int(end_color[3:5], 16), int(end_color[5:7], 16)
+    r = int(r1 + fraction * (r2 - r1))
+    g = int(g1 + fraction * (g2 - g1))
+    b = int(b1 + fraction * (b2 - b1))
+    return f'#{r:02X}{g:02X}{b:02X}'
+
 # Create the main application window
 app = tk.Tk()
 app.title("The Eloquent Reader")
@@ -39,28 +48,38 @@ pdf_path = tk.StringVar()
 speech_rate = tk.StringVar()
 speech_rate.set(150)
 
-# Define colors for styling
-bg_color = "#F6F7F8"
-label_color = "#333333"
-button_color = "#6AC7F3"
+# Create a canvas for the gradient background
+canvas = tk.Canvas(app, width=400, height=300)
+canvas.pack()
+
+# Define gradient colors
+color1 = "#6AC7F3"  # Top color
+color2 = "#224D92"  # Bottom color
+
+# Create a gradient rectangle
+canvas.create_rectangle(0, 0, 400, 300, fill="", outline="")
+for i in range(300):
+    y0 = i
+    y1 = i + 1
+    canvas.create_rectangle(0, y0, 400, y1, fill=gradient_color(color1, color2, i / 300), outline="")
 
 # Create and configure widgets
-file_label = tk.Label(app, text="Select PDF File:", bg=bg_color, fg=label_color)
+file_label = tk.Label(app, text="Select PDF File:", bg=gradient_color(color1, color2, 0.3), fg="white")
 file_entry = tk.Entry(app, textvariable=pdf_path, width=40)
-browse_button = tk.Button(app, text="Browse", command=browse_pdf, bg=button_color, fg="white")
-rate_label = tk.Label(app, text="Speech Rate (words per minute):", bg=bg_color, fg=label_color)
+browse_button = tk.Button(app, text="Browse", command=browse_pdf, bg=gradient_color(color1, color2, 0.3), fg="white")
+rate_label = tk.Label(app, text="Speech Rate (words per minute):", bg=gradient_color(color1, color2, 0.3), fg="white")
 rate_entry = tk.Entry(app, textvariable=speech_rate)
-convert_button = tk.Button(app, text="Convert and Speak", command=convert_and_speak, bg=button_color, fg="white")
-result_label = tk.Label(app, text="", bg=bg_color, fg="red")
+convert_button = tk.Button(app, text="Convert and Speak", command=convert_and_speak, bg=gradient_color(color1, color2, 0.3), fg="white")
+result_label = tk.Label(app, text="", bg=gradient_color(color1, color2, 0.3), fg="red")
 
 # Place widgets in the window
-file_label.grid(row=0, column=0, padx=10, pady=10)
-file_entry.grid(row=0, column=1, padx=10, pady=10)
-browse_button.grid(row=0, column=2, padx=10, pady=10)
-rate_label.grid(row=1, column=0, padx=10, pady=10)
-rate_entry.grid(row=1, column=1, padx=10, pady=10)
-convert_button.grid(row=2, column=1, padx=10, pady=10)
-result_label.grid(row=3, column=1, padx=10, pady=10)
+file_label.pack(pady=10)
+file_entry.pack(pady=10)
+browse_button.pack(pady=10)
+rate_label.pack(pady=10)
+rate_entry.pack(pady=10)
+convert_button.pack(pady=10)
+result_label.pack(pady=10)
 
 # Start the main loop
 app.mainloop()
